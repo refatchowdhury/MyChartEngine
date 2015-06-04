24e556a898b4e37b1be0e6cea3cd6f09566bf04f
MR Chowdhury <refat_chowdhury@yahoo.com>
2015-06-04 16:39:38 +0200

    first commit

---
+++ src/com/example/mychartengine/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.mychartengine;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//From http://android-graphview.org
+		  //init example series data
+		GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
+		    new GraphViewData(1, 2.0d)
+		    , new GraphViewData(2, 1.5d)
+		    , new GraphViewData(3, 2.5d)
+		    , new GraphViewData(4, 1.0d)
+		});
+		 
+		GraphView graphView = new LineGraphView(
+		    this // context
+		    , "GraphViewDemo" // heading
+		);
+		graphView.addSeries(exampleSeries); // data
+		 
+		LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
+		layout.addView(graphView);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
